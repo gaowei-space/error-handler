2514400764136e8dac6fff71773f972099012116
gaowei <huyao9950@hotmail.com>
2022-08-01 17:20:57 +0800

    fix: edit style

--- examples/Monolog.php
+++ examples/Monolog.php
@@ -5,6 +5,6 @@ use GaoweiSpace\ErrorHandler\Examples\ErrorHandlerExample;
 require __DIR__.'/ErrorHandlerExample.php';
 
 $test = new ErrorHandlerExample();
-$test->testInitLogger(3);
 
+$test->testInitLogger(3);
 $test->testCaptureMessageForLogger('测试', 'error');
--- examples/Sentry.php
+++ examples/Sentry.php
@@ -5,6 +5,6 @@ use GaoweiSpace\ErrorHandler\Examples\ErrorHandlerExample;
 require __DIR__.'/ErrorHandlerExample.php';
 
 $test = new ErrorHandlerExample();
-$test->testInitForSentry(3);
 
+$test->testInitForSentry(3);
 $test->testCaptureMessageForSentry('测试', 'error');
